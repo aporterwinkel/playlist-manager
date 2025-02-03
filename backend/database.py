9e45dfd9d1021e6409aa051e4e6d3fb03f8cc680
Aaron Porterwinkel <aporterwinkel@gmail.com>
2025-02-03 17:13:42 -0600

    parameterize DB file

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+import dotenv
+
+dotenv.load_dotenv(override=True)
 
 Base = declarative_base()
 
@@ -12,7 +15,7 @@ class Database:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Database, cls).__new__(cls)
-            DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./music_files.db")
+            DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////data/playlists.db")
             cls._engine = create_engine(DATABASE_URL)
             cls._sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=cls._engine)
             Base.metadata.create_all(bind=cls._engine)
